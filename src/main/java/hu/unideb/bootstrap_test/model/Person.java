cf382ac6929c3a7149c16b67a826ce4e9b0aa192
makar <makar.peter98@gmail.com>
2024-11-12 08:38:03 +0100

    index.html

--- src/main/java/hu/unideb/bootstrap_test/model/Person.java
+++ src/main/java/hu/unideb/bootstrap_test/model/Person.java
@@ -9,13 +9,19 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "persons")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Person {
-//    @Id
-//    @GeneratedValue
-//    private Integer id;
-//    private String firstName;
-//    private String lastName;
-//    private String email;
-//    private String password;
-//    private boolean enabled;
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private boolean enabled;
 }
