d8b3e5b20904a3bd33d5f823e52409eb52c770b4
makar <makar.peter98@gmail.com>
2024-11-19 08:36:02 +0100

    11. ora init commit

--- src/main/java/hu/unideb/bootstrap_test/BootstrapTestApplication.java
+++ src/main/java/hu/unideb/bootstrap_test/BootstrapTestApplication.java
@@ -9,6 +9,7 @@ public class BootstrapTestApplication {
     public static void main(String[] args) {
         SpringApplication.run(BootstrapTestApplication.class, args);
         //Test komment!
+        //Test komment 2!
     }
 
 }
