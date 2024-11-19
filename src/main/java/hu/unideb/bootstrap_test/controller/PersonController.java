0f94a958012a254e38fc86ae36ab19315852e6e7
makar <makar.peter98@gmail.com>
2024-11-19 08:55:56 +0100

    11. ora new person működik

--- src/main/java/hu/unideb/bootstrap_test/controller/PersonController.java
+++ src/main/java/hu/unideb/bootstrap_test/controller/PersonController.java
@@ -26,43 +26,43 @@ public class PersonController {
         model.addAttribute("personsList", persons);
         return "persons";
     }
-//
-//    @GetMapping("/persons/new")
-//    public String newPerson(Model model){
-//        model.addAttribute("person", new Person());
-//        model.addAttribute("pageTitle", "Add new person");
-//        redirectText="Add successfully";
-//        return "newPersonFrom";
-//    }
-//
-//    @PostMapping("/persons/save")
-//    public String savePerson(Person person, RedirectAttributes rd){
-//        personRepository.save(person);
-//        rd.addFlashAttribute("message", redirectText);
-//        return "redirect:/persons";
-//    }
-//
-//    @GetMapping("/persons/delete/{id}")
-//    public String deletePerson(@PathVariable Integer id, RedirectAttributes rd) {
-//        Optional<Person> personToDelete = personRepository.findById(id);
-//        if (personToDelete.isPresent()) {
-//            personRepository.delete(personToDelete.get());
-//            rd.addFlashAttribute("message", "Delete operation successful");
-//        }
-//        return "redirect:/persons";
-//    }
-//
-//    @GetMapping("/persons/edit/{id}")
-//    public String editPerson(@PathVariable Integer id, Model model){
-//        Optional<Person> personToEdit = personRepository.findById(id);
-//        if (personToEdit.isPresent()) {
-//            model.addAttribute("person", personToEdit.get());
-//            model.addAttribute("pageTitle", "Edit person with id: " + id);
-//            redirectText="Changed successfully";
-//            return "newPersonFrom";
-//        }
-//        return "redirect:/persons";
-//    }
+
+    @GetMapping("/persons/new")
+    public String newPerson(Model model){
+        model.addAttribute("person", new Person());
+        model.addAttribute("pageTitle", "Add new person");
+        redirectText="Add successfully";
+        return "newPersonFrom";
+    }
+
+    @PostMapping("/persons/save")
+    public String savePerson(Person person, RedirectAttributes rd){
+        personRepository.save(person);
+        rd.addFlashAttribute("message", redirectText);
+        return "redirect:/persons";
+    }
+
+    @GetMapping("/persons/delete/{id}")
+    public String deletePerson(@PathVariable Integer id, RedirectAttributes rd) {
+        Optional<Person> personToDelete = personRepository.findById(id);
+        if (personToDelete.isPresent()) {
+            personRepository.delete(personToDelete.get());
+            rd.addFlashAttribute("message", "Delete operation successful");
+        }
+        return "redirect:/persons";
+    }
+
+    @GetMapping("/persons/edit/{id}")
+    public String editPerson(@PathVariable Integer id, Model model){
+        Optional<Person> personToEdit = personRepository.findById(id);
+        if (personToEdit.isPresent()) {
+            model.addAttribute("person", personToEdit.get());
+            model.addAttribute("pageTitle", "Edit person with id: " + id);
+            redirectText="Changed successfully";
+            return "newPersonFrom";
+        }
+        return "redirect:/persons";
+    }
 
 
 }
